a2b2c10dad98bf26cbef81ab12e2645bbc5caf16
Daniel Åslund <daniel.aaslund@gmail.com>
2019-08-31 00:56:51 +0200

    Cleaned up try-catch block and added comments

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -8,10 +8,14 @@ namespace LeapYear
         {
             int userYear = 0;
             int currentYear = DateTime.Today.Year;
+
+            // bool variable to control the input loop
             bool loop = false;
 
+            // do loop for the user input
             do
             {
+                // Reset bool to assume that all is OK
                 if (loop)
                 {
                     loop = false;
@@ -21,18 +25,20 @@ namespace LeapYear
                 try
                 {
                     userYear = Int32.Parse(Console.ReadLine());
-
-                    if (userYear >= currentYear)
-                    {
-                        Console.WriteLine("That is not past year.");
-                        loop = true;
-                    }
                 }
+                // Check for FormatException on the input to verify that the user has entered a number
                 catch (FormatException)
                 {
                     Console.WriteLine("That is not a proper year.\n\rPlease write it as a number.");
                     loop = true;
                 }
+
+                if (userYear >= currentYear)
+                    {
+                        Console.WriteLine("That is not past year.");
+                        loop = true;
+                    }
+
             } while (loop);
 
             Console.WriteLine("\n\rHere are all the Leap Years between years {0} and {1}:", userYear, currentYear);
